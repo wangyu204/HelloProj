e345ccbaaf08c37d1a90cf25e489e39a72e9d3e2
王喻 <wangyu@317hu.com>
2019-11-27 15:18:54 +0800

    修改内容：字典遍历

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,7 +1,17 @@
 # coding=utf-8
 
 
-# 集合 {}
-n_set = {x for x in range(100) if x % 2 == 0 if x % 5 == 0}
-print(n_set)
-print(type(n_set))
+# 字典 {key1:value1, key2:value2}
+student_dic = {101: "wangyu", 102: "lisue", 103: "huanghaipo"}
+
+print('---key---')
+for student_id in student_dic.keys():
+    print("学号：" + str(student_id))
+
+print('---value---')
+for student_name in student_dic.values():
+    print("名字：" + str(student_name))
+
+print('------')
+for student_id, student_name in student_dic.items():
+    print("学号：{0} - 名字：{1}".format(student_id, student_name))
