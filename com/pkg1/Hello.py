648479f035158b9428ae689af4e0afcb64d8117b
王喻 <wangyu@317hu.com>
2019-11-27 18:58:38 +0800

    修改内容：添加finally的异常捕获

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -2,19 +2,26 @@
 
 
 import datetime as dt
-import traceback as tb
 
 
 def read_date_from_file(filename):
     try:
         file = open(filename)
-        in_date = file.read()
-        in_date = in_date.strip()
-        date = dt.datetime.strptime(in_date, '%Y-%m-%d')
-        return date
-    except (ValueError, OSError) as e:
-        print('调用方法method1处理...')
-        tb.print_exc()
+    except OSError as e:
+        print('打开文件失败')
+    else:
+        print('打开文件成功')
+        try:
+            in_date = file.read()
+            in_date = in_date.strip()
+            date = dt.datetime.strptime(in_date, '%Y-%m-%d')
+            return date
+        except ValueError as e:
+            print('处理ValueError异常')
+        except OSError as e:
+            print('处理OSError异常')
+        finally:
+            file.close()
 
 
 date = read_date_from_file('readme.txt')
