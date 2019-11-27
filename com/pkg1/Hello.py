a125ff1b853ca05f9ccd95329cecebc095977d7c
王喻 <wangyu@317hu.com>
2019-11-27 15:27:13 +0800

    修改内容：多种方式函数调用

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,12 +1,13 @@
 # coding=utf-8
 
 
-# 函数
-def rectangle_area(width, height):
+def print_area(width, height):
     area = width * height
-    return area
+    print("{0} x {1} 长方形的面积:{2}".format(width, height, area))
 
 
-r_area = rectangle_area(320.0, 480.0)
-
-print("320*480={0:.2f}".format(r_area))
+print_area(320.0, 480.0)  # 没有采用关键字参数函数调用
+print_area(width=320.0, height=480.0)  # 采用关键字参数函数调用
+print_area(320.0, height=480.0)  # 采用关键字参数函数调用
+# print_area(width=320.0, height)  # 发生错误
+print_area(height=480.0, width=320.0)  # 采用关键字参数函数调用
