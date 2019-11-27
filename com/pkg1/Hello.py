0f49f8cd122f2927b88bc4d7826de7b2527fed55
王喻 <wangyu@317hu.com>
2019-11-27 16:26:05 +0800

    修改内容：三大基础函数

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,16 +1,28 @@
 # coding=utf-8
 
-def calculate_fun(opr):
-    if opr == '+':
-        return lambda a, b: (a + b)
-    else:
-        return lambda a, b: (a - b)
+from functools import reduce
 
+# 1.filter()  过滤
+users = ['Tony', 'Tom', 'Ben', 'Alex']
 
-f1 = calculate_fun('+')
-f2 = calculate_fun('-')
+users_filter = filter(lambda u: u.startswith('T'), users)
+print(list(users_filter))
 
-print(type(f1))
+number_list = range(1, 11)
+nmber_filter = filter(lambda it: it % 2 == 0, number_list)
+print(list(nmber_filter))
 
-print("10 + 5 = {0}".format(f1(10, 5)))
-print("10 - 5 = {0}".format(f2(10, 5)))
+# 2.map()  映射
+users_map = map(lambda u: u.lower(), users)
+print(list(users_map))
+
+# users_map = map(lambda u: u.lower(), users_filter)
+users_map = map(lambda u: u.lower(), filter(lambda u: u.startswith('T'), users))
+
+print(list(users_map))
+
+# 3.reduce()  聚合
+a = (1, 2, 3, 4)
+a_reduce = reduce(lambda acc, i: acc + i, a)  # 10
+# a_reduce = reduce(lambda acc, i: acc + i, a, 2)  # 12
+print(a_reduce)
