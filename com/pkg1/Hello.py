f9ec08c5631cba8f0bb1baec3d2ac087eee7ed27
王喻 <wangyu@317hu.com>
2019-11-27 15:10:12 +0800

    修改内容：集合 {}

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,15 +1,12 @@
 # coding=utf-8
 
 
-# 列表 list [] 也是一种序列结构
-n_list = []
-for x in range(10):
-    if x % 2 == 0:
-        n_list.append(x ** 2)
-print(n_list)
+# 集合 {}
+student_set = {'张三', '李四', '王五'}
 
-n_list = [x ** 2 for x in range(10) if x % 2 == 0]
-print(n_list)
+for item in student_set:
+    print(item)
 
-n_list = [x for x in range(100) if x % 2 == 0 if x % 5 == 0]
-print(n_list)
+print('-----------')
+for i, item in enumerate(student_set):
+    print('{0} - {1}'.format(i, item))
