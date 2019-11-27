b27c4c47002599925770fc98e583c86e23de2d9c
王喻 <wangyu@317hu.com>
2019-11-27 18:38:05 +0800

    修改内容：异常捕获

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,6 +1,18 @@
 # coding=utf-8
 
 
-i = input('请输入数字：')
-print(i)
-print(5 / int(i))
+import datetime as dt
+
+
+def read_date(in_date):
+    try:
+        tmpDate = dt.datetime.strptime(str_date, '%Y-%m-%d')
+        return tmpDate
+    except ValueError as e:
+        print('处理ValueError异常')
+        print(e)
+
+
+# 非法时间 直接调用会报异常
+str_date = '2011-13-18'
+print('日期 = {0}'.format(read_date(str_date)))
