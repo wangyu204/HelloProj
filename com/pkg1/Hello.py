a12cfa8de1d134e1b863e5d5776e2d521f5074a4
王喻 <wangyu@317hu.com>
2019-11-27 15:34:31 +0800

    修改内容：元组实现多值返回

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,31 +1,12 @@
 # coding=utf-8
 
-# 可变参数 一个* 默认组装成元组，两个** 组装字典
-def sum(*numbers, multiple=1):
-    """定义*可变参数函数"""
-    total = 0.0
-    for number in numbers:
-        total += number
-    return total * multiple
+# 元组实现 多值返回
+def position(dt, speed):
+    posx = speed[0] * dt
+    posy = speed[1] * dt
+    return (posx, posy)
 
 
-print(sum(100.0, 20.0, 30.0))  # 输出150.0
-print(sum(30.0, 80.0))  # 输出110.0
-print(sum(30.0, 80.0, multiple=2))  # 输出220.0
+move = position(60.0, (10, -5))
 
-double_tuple = (50.0, 60.0, 0.0)  # 元组或列表
-print(sum(30.0, 80.0, *double_tuple))  # 输出220.0
-
-
-def show_info(sep=':', **info):
-    """定义**可变参数函数"""
-    print('-----info------')
-    for key, value in info.items():
-        print('{0} {2} {1}'.format(key, value, sep))
-
-
-show_info('->', name='Tony', age=18, sex=True)
-show_info(sutdent_name='Tony', sutdent_no='1000', sep='-')
-
-stu_dict = {'name': 'Tony', 'age': 18}  # 创建字典对象
-show_info(**stu_dict, sex=True, sep='=')  # 传递字典stu_dict
+print("物体位移：({0}, {1})".format(move[0], move[1]))
