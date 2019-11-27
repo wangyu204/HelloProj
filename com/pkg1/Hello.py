5239178ef31201b18ff6ceb95eebaa27b07551f9
王喻 <wangyu@317hu.com>
2019-11-27 17:21:19 +0800

    修改内容：继承使用

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,44 +1,45 @@
 # coding=utf-8
 
 
-# class Animal(object):
-#     """定义动物类"""
+class Person:
+
+    def __init__(self, name, age):
+        self.name = name  # 名字
+        self.age = age  # 年龄
+
+    def info(self):
+        template = 'Person [name={0}, age={1}]'
+        s = template.format(self.name, self.age)
+        return s
+
+
+p = Person('小赵', 18)
+print(p.info())
+
+
+# class Student:
 #
-#     def __init__(self, age, sex=1, weight=0.0):
-#         self.age = age  # 定义年龄实例成员变量
-#         self.sex = sex  # 定义性别实例成员变量
-#         self.__weight = weight  # 定义体重实例成员变量
+#     def __init__(self, name, age, school):
+#         self.name = name  # 名字
+#         self.age = age  # 年龄
+#         self.school = school  # 所在学校
 #
-#     def get_weight(self):
-#         return self.__weight
-#
-#     def set_weight(self, weight):
-#         self.__weight = weight
-#
-#
-# a1 = Animal(2, 0, 10.0)
-# print('a1体重：{0:0.2f}'.format(a1.get_weight()))
-# a1.set_weight(123.45)
-# print('a1体重：{0:0.2f}'.format(a1.get_weight()))
+#     def info(self):
+#         template = 'Student [name={0}, age={1}, school={2}]'
+#         s = template.format(self.name, self.age, self.school)
+#         return s
 
-class Animal(object):
-    """定义动物类"""
+class Student(Person):
 
-    def __init__(self, age, sex=1, weight=0.0):
-        self.age = age  # 定义年龄实例成员变量
-        self.sex = sex  # 定义性别实例成员变量
-        self.__weight = weight  # 定义体重实例成员变量
+    def __init__(self, name, age, school):
+        super().__init__(name, age)
+        self.school = school  # 所在学校
 
-    @property
-    def weight(self):  # 替代get_weight(self):
-        return self.__weight
-
-    @weight.setter
-    def weight(self, weight):  # 替代set_weight(self, weight):
-        self.__weight = weight
+    # def info(self):
+    #     template = 'Student [name={0}, age={1}, school={2}]'
+    #     s = template.format(self.name, self.age, self.school)
+    #     return s
 
 
-a1 = Animal(2, 0, 10.0)
-print('a1体重：{0:0.2f}'.format(a1.weight))
-a1.weight = 123.45  # a1.set_weight(123.45)
-print('a1体重：{0:0.2f}'.format(a1.weight))
+s = Student('Tom', 28, '清华大学')
+print(s.info())
