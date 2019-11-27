8ce80ed9b74e92d4ce2b0fa5f24a5c729e92b0ad
王喻 <wangyu@317hu.com>
2019-11-27 13:24:07 +0800

    修改内容：if elif else语句

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -2,7 +2,17 @@
 
 import sys
 
-score = int(sys.argv[1]) # 获得命令行传入的参数
+score = int(sys.argv[1])  # 获得命令行传入的参数
 
-if score >=85:
-    print("优秀")
+if score >= 90:
+    grade = 'A'
+elif score >= 80:
+    grade = 'B'
+elif score >= 70:
+    grade = 'C'
+elif score >= 60:
+    grade = 'D'
+else:
+    grade = 'F'
+
+print("Grade = " + grade)
