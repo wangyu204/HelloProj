64c7a404ad9935ebab194abab800d896a0e3a4aa
王喻 <wangyu@317hu.com>
2019-11-27 16:29:55 +0800

    修改内容：类定义

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,28 +1,10 @@
 # coding=utf-8
 
-from functools import reduce
+# 类  基础父类
+class Animal(object):
+    # 类体
+    pass  # pass语句只是 占位的作用
 
-# 1.filter()  过滤
-users = ['Tony', 'Tom', 'Ben', 'Alex']
 
-users_filter = filter(lambda u: u.startswith('T'), users)
-print(list(users_filter))
-
-number_list = range(1, 11)
-nmber_filter = filter(lambda it: it % 2 == 0, number_list)
-print(list(nmber_filter))
-
-# 2.map()  映射
-users_map = map(lambda u: u.lower(), users)
-print(list(users_map))
-
-# users_map = map(lambda u: u.lower(), users_filter)
-users_map = map(lambda u: u.lower(), filter(lambda u: u.startswith('T'), users))
-
-print(list(users_map))
-
-# 3.reduce()  聚合
-a = (1, 2, 3, 4)
-a_reduce = reduce(lambda acc, i: acc + i, a)  # 10
-# a_reduce = reduce(lambda acc, i: acc + i, a, 2)  # 12
-print(a_reduce)
+animal = Animal()
+print(animal)
