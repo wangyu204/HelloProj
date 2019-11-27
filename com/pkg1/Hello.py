5fe2467ef94cb8bd518f748d51d6d7e8729d789f
王喻 <wangyu@317hu.com>
2019-11-27 18:42:50 +0800

    修改内容：多重异常捕获1

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -4,15 +4,23 @@
 import datetime as dt
 
 
-def read_date(in_date):
+def read_date_from_file(filename):
     try:
-        tmpDate = dt.datetime.strptime(str_date, '%Y-%m-%d')
-        return tmpDate
+        file = open(filename)
+        in_date = file.read()
+        in_date = in_date.strip()
+        date = dt.datetime.strptime(in_date, '%Y-%m-%d')
+        return date
     except ValueError as e:
         print('处理ValueError异常')
         print(e)
+    except FileNotFoundError as e:
+        print('处理FileNotFoundError异常')
+        print(e)
+    except OSError as e:
+        print('处理OSError异常')
+        print(e)
 
 
-# 非法时间 直接调用会报异常
-str_date = '2011-13-18'
-print('日期 = {0}'.format(read_date(str_date)))
+date = read_date_from_file('readme.txt')
+print('日期 = {0}'.format(date))
