dd2ea61b84a1bb3d85f3b7a8fa27b14348667e69
王喻 <wangyu@317hu.com>
2019-11-27 16:11:53 +0800

    修改内容：生成器  yield返回一个可迭代的对象

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,15 +1,10 @@
 # coding=utf-8
 
-
-# 创建全局变量x
-x = 20
-
-
-def print_value():
-    global x
-    x = 10
-    print("函数中x = {0}".format(x))
+# 生成器  yield   不直接返回所有数据，而是返回一个生成器对象（一种可迭代的对象）  __next__()
+def square(num):
+    for i in range(1, num + 1):
+        yield i * i
 
 
-print_value()
-print("全局变量x = {0}".format(x))
+for i in square(5):
+    print(i, end=' ')
