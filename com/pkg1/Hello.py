251cf7dfbc7d7d7722d276c29d654d4b36de61d6
王喻 <wangyu@317hu.com>
2019-11-27 19:03:56 +0800

    修改内容：更高端用发，替换finally

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -6,22 +6,16 @@ import datetime as dt
 
 def read_date_from_file(filename):
     try:
-        file = open(filename)
-    except OSError as e:
-        print('打开文件失败')
-    else:
-        print('打开文件成功')
-        try:
+        with open(filename) as file:
             in_date = file.read()
-            in_date = in_date.strip()
-            date = dt.datetime.strptime(in_date, '%Y-%m-%d')
-            return date
-        except ValueError as e:
-            print('处理ValueError异常')
-        except OSError as e:
-            print('处理OSError异常')
-        finally:
-            file.close()
+
+        in_date = in_date.strip()
+        date = dt.datetime.strptime(in_date, '%Y-%m-%d')
+        return date
+    except ValueError as e:
+        print('处理ValueError异常')
+    except OSError as e:
+        print('处理OSError异常')
 
 
 date = read_date_from_file('readme.txt')
