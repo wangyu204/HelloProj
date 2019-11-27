036597a287c7edd371013835c7343ead7854d2bc
王喻 <wangyu@317hu.com>
2019-11-27 19:17:53 +0800

    修改内容：随机数生成

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,28 +1,30 @@
 # coding=utf-8
 
 
-import datetime as dt
+import random
 
+# 0.0 <= x < 1.0随机数
+print('----0.0 <= x < 1.0随机数----')
+for i in range(0, 10):
+    x = random.random()
+    print(x)
 
-class MyException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
+# 0 <= x < 5随机数
+print('----0 <= x < 5随机数----')
+for i in range(0, 10):
+    x = random.randrange(5)
+    print(x, end=' ')
 
+# 5 <= x < 10随机数
+print()
+print('----5 <= x < 10随机数----')
+for i in range(0, 10):
+    x = random.randrange(5, 10)
+    print(x, end=' ')
 
-def read_date_from_file(filename):
-    try:
-        file = open(filename)
-        in_date = file.read()
-        in_date = in_date.strip()
-        date = dt.datetime.strptime(in_date, '%Y-%m-%d')
-        return date
-    except ValueError as e:
-        raise MyException('不是有效的日期')
-    except FileNotFoundError as e:
-        raise MyException('文件找不到')
-    except OSError as e:
-        raise MyException('文件无法打开或无法读取')
-
-
-date = read_date_from_file('readme.txt')
-print('日期 = {0}'.format(date))
+# 5 <= x <= 10随机数
+print()
+print('----5 <= x <= 10随机数----')
+for i in range(0, 10):
+    x = random.randint(5, 10)
+    print(x, end=' ')
