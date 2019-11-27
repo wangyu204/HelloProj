ee4efae414e3b1a95adb57b409734bf33b536f9d
王喻 <wangyu@317hu.com>
2019-11-27 11:19:17 +0800

    修改内容：位运算和格式化输出

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,9 +1,16 @@
-print("hello word")
+# coding=utf-8
 
-i = 0
-a = 10
-b = 9
-if a > b or i == 1:
-    print("right")
-else:
-    print("wrong")
+
+a = 0b10110010
+b = 0b01011110
+
+print("a | b = {0:b}".format(a | b))  # 0b11111110
+print("a & b = {0}".format(a & b))  # 0b00010010
+print("a ^ b = {0}".format(a ^ b))  # 0b11101100
+print("~a = {0}".format(~a))  # -179
+print("a >> 2 = {0}".format(a >> 2))  # 0b00101100
+print("a << 2 = {0}".format(a << 2))  # 0b11001000
+
+c = -0b1100
+print("c >> 2 = {0}".format(c >> 2))  # -0b00000011
+print("c <<  2 = {0}".format(c << 2))  # -0b00110000
