7a12284c0be52f39de213065d93d852cd707a982
王喻 <wangyu@317hu.com>
2019-11-28 16:16:21 +0800

    修改内容：二进制文件读写  将a文件内容全部复制到b文件中

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,12 +1,12 @@
 # coding=utf-8
 
 
-f_name = 'test.txt'
+f_name = 'coco2dxcplus.jpg'
 
-with open(f_name, 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-    print(lines)
-    copy_f_name = 'copy.txt'
-    with open(copy_f_name, 'w', encoding='utf-8') as copy_f:
-        copy_f.writelines(lines)
+with open(f_name, 'rb') as f:
+    b = f.read()
+    print(type(b))
+    copy_f_name = 'copy.jpg'
+    with open(copy_f_name, 'wb') as copy_f:
+        copy_f.write(b)
         print('文件复制成功')
