68dd9bf4e900980a21775fe2caf5cbaf1804bc71
王喻 <wangyu@317hu.com>
2019-11-28 10:46:17 +0800

    修改内容：正则表达式 量词使用（惰性量词）

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -3,32 +3,10 @@
 
 import re
 
-m = re.search(r'\d?', '87654321')  # 出现数字一次
-print(m)  # 匹配字符'8'
-
-m = re.search(r'\d?', 'ABC')  # 出现数字零次
-print(m)  # 匹配字符''
-
-m = re.search(r'\d*', '87654321')  # 出现数字多次
+# 使用贪婪量词
+m = re.search(r'\d{5,8}', '87654321')  # 出现数字8次
 print(m)  # 匹配字符'87654321'
 
-m = re.search(r'\d*', 'ABC')  # 出现数字零次
-print(m)  # 匹配字符''
-
-m = re.search(r'\d+', '87654321')  # 出现数字多次
-print(m)  # 匹配字符'87654321'
-
-m = re.search(r'\d+', 'ABC')
-print(m)  # 不匹配
-
-m = re.search(r'\d{8}', '87654321')  # 出现数字8次
-print('8765432', m)  # 匹配字符'87654321'
-
-m = re.search(r'\d{8}', 'ABC')
-print(m)  # 不匹配
-
-m = re.search(r'\d{7,8}', '87654321')  # 出现数字8次
-print(m)  # 匹配字符'87654321'
-
-m = re.search(r'\d{9,}', '87654321')
-print(m)  # 不匹配
+# 使用惰性量词
+m = re.search(r'\d{5,8}?', '87654321')  # 出现数字5次
+print(m)  # 匹配字符'87654'
