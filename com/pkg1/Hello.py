edc7aeab9a2c338815bfdfea29ae31fd8952277a
王喻 <wangyu@317hu.com>
2019-11-28 15:44:30 +0800

    修改内容：正则表达式 compile() 多行模式， 对 ^ $   开始和结束会有影响

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -3,13 +3,13 @@
 
 import re
 
-p = r'.+'
+p = r'^World'
 regex = re.compile(p)
 
 m = regex.search('Hello\nWorld.')
-print(m)  # 匹配
+print(m)  # 不匹配
 
-regex = re.compile(p, re.DOTALL)
+regex = re.compile(p, re.M)
 
 m = regex.search('Hello\nWorld.')
 print(m)  # 匹配
