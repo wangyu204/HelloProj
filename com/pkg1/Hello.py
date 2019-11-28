ed6e266adc3fd80966b6c0392856cb83cad20449
王喻 <wangyu@317hu.com>
2019-11-28 10:53:38 +0800

    修改内容：正则表达式 分组使用

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -3,10 +3,15 @@
 
 import re
 
-# 使用贪婪量词
-m = re.search(r'\d{5,8}', '87654321')  # 出现数字8次
-print(m)  # 匹配字符'87654321'
+p = r'(121){2}'
+m = re.search(p, '121121abcabc')
+print(m)  # 匹配
+print(m.group())  # 返回匹配字符串
+print(m.group(1))  # 获得第一组内容  分组默认从1开始
+print(m.groups())  # 获得所有组内容
 
-# 使用惰性量词
-m = re.search(r'\d{5,8}?', '87654321')  # 出现数字5次
-print(m)  # 匹配字符'87654'
+p = r'(\d{3,4})-(\d{7,8})'
+m = re.search(p, '010-87654321')
+print(m)  # 匹配
+print(m.group())  # 返回匹配字符串
+print(m.groups())  # 获得所有组内容  一个小括号就是1组
