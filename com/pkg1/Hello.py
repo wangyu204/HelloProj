c30e8b6c8897e3ca466ed9a44d834f7041f27787
王喻 <wangyu@317hu.com>
2019-11-28 15:46:41 +0800

    修改内容：正则表达式 compile() 详细模式，很方便阅读 re.VERBOSE

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -3,13 +3,18 @@
 
 import re
 
-p = r'^World'
-regex = re.compile(p)
+p = """(java)     #匹配java字符串
+        .*        #匹配任意字符零或多个
+        (python)  #匹配python字符串
+    """
 
-m = regex.search('Hello\nWorld.')
-print(m)  # 不匹配
+regex = re.compile(p, re.I | re.VERBOSE)
 
-regex = re.compile(p, re.M)
+m = regex.search('I like Java and Python.')
+print(m)  # 匹配
 
-m = regex.search('Hello\nWorld.')
+m = regex.search('I like JAVA and Python.')
+print(m)  # 匹配
+
+m = regex.search('I like java and Python.')
 print(m)  # 匹配
