38a54109ef8392eb00058c69c69874c1a99e5700
王喻 <wangyu@317hu.com>
2019-11-28 15:38:07 +0800

    修改内容：正则表达式 compile() 忽略大小写

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -3,21 +3,14 @@
 
 import re
 
-text = '你们好Hello'
+p = r'(java).*(python)'
+regex = re.compile(p, re.I)
 
-p = r'\w+'
-regex = re.compile(p, re.U)
-
-m = regex.search(text)
+m = regex.search('I like Java and Python.')
 print(m)  # 匹配
 
-m = regex.match(text)
+m = regex.search('I like JAVA and Python.')
 print(m)  # 匹配
 
-regex = re.compile(p, re.A)
-
-m = regex.search(text)
+m = regex.search('I like java and Python.')
 print(m)  # 匹配
-
-m = regex.match(text)
-print(m)  # 不匹配
