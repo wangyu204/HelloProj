a6e89fdf45f10ddf6f57b4eed0f8db3d33b74a68
王喻 <wangyu@317hu.com>
2019-11-28 15:32:29 +0800

    修改内容：正则表达式 compile()编译正在表达式提高性能

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -3,14 +3,33 @@
 
 import re
 
+p = r'\w+@zhijieketang\.com'
+regex = re.compile(p)
+
+text = "Tony's email is tony_guan588@zhijieketang.com."
+m = regex.search(text)
+print(m)  # 匹配
+
+m = regex.match(text)
+print(m)  # 不匹配
+
+p = r'[Jj]ava'
+regex = re.compile(p)
+text = 'I like Java and java.'
+
+match_list = regex.findall(text)
+print(match_list)  # 匹配
+
+match_iter = regex.finditer(text)
+for m in match_iter:
+    print(m.group())
+
 p = r'\d+'
+regex = re.compile(p)
 text = 'AB12CD34EF'
 
-repace_text = re.sub(p, ' ', text)
-print(repace_text)
+clist = regex.split(text)
+print(clist)
 
-repace_text = re.sub(p, ' ', text, count=1)
-print(repace_text)
-
-repace_text = re.sub(p, ' ', text, count=2)
+repace_text = regex.sub(' ', text)
 print(repace_text)
