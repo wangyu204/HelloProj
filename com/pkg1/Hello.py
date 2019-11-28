bb93813bdbcf7a8030309c1851f4ad061a096044
王喻 <wangyu@317hu.com>
2019-11-28 16:08:36 +0800

    修改内容：关闭文件close() 一般用不到，如果你用 with as

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,15 +1,23 @@
 # coding=utf-8
 
 
-f = open('test.txt', 'w+')
-f.write('world222')
+# 使用finally关闭文件
+f_name = 'test.txt'
+try:
+    f = open(f_name)
+except OSError as e:
+    print('打开文件失败')
+else:
+    print('打开文件成功')
+    try:
+        content = f.read()
+        print(content)
+    except OSError as e:
+        print('处理OSError异常')
+    finally:
+        f.close()
 
-f = open('test.txt', 'r+')
-f.write('Hello')
-
-f = open('test.txt', 'a')
-f.write(' ')
-
-fName = r'test.txt'
-f = open(fName, 'a+')
-f.write('World')
+# 使用with as自动资源管理
+with open(f_name, 'r') as f:
+    content = f.read()
+    print(content)
