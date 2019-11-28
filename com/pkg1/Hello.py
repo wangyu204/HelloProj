32a07f190adb37f0cda8d7645cee3bec4ca74348
王喻 <wangyu@317hu.com>
2019-11-28 11:11:23 +0800

    修改内容：正则表达式 search() match()

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -3,14 +3,25 @@
 
 import re
 
-s = 'img1.jpg,img2.jpg,img3.bmp'
+p = r'\w+@zhijieketang\.com'
 
-# 捕获分组
-p = r'\w+(\.jpg)'
-mlist = re.findall(p, s)
-print(mlist)
+text = "Tony's email is tony_guan588@zhijieketang.com."
+m = re.search(p, text)
+print(m)  # 匹配
 
-# 非捕获分组
-p = r'\w+(?:\.jpg)'
-mlist = re.findall(p, s)
-print(mlist)
+m = re.match(p, text)
+print(m)  # 不匹配
+
+email = 'tony_guan588@zhijieketang.com'
+m = re.search(p, email)
+print(m)  # 匹配
+
+m = re.match(p, email)
+print(m)  # 匹配
+
+# match对象几个方法
+print('match对象几个方法:')
+print(m.group())  # tony_guan588@zhijieketang.com
+print(m.start())  # 0
+print(m.end())  # 29
+print(m.span())  # (0, 29)
