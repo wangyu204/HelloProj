96e726d22a05db5dbde1be67994c2fc05397a885
王喻 <wangyu@317hu.com>
2019-11-28 10:55:45 +0800

    修改内容：正则表达式 分组组名使用

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -3,15 +3,16 @@
 
 import re
 
-p = r'(121){2}'
-m = re.search(p, '121121abcabc')
-print(m)  # 匹配
-print(m.group())  # 返回匹配字符串
-print(m.group(1))  # 获得第一组内容  分组默认从1开始
-print(m.groups())  # 获得所有组内容
-
-p = r'(\d{3,4})-(\d{7,8})'
+p = r'(?P<area_code>\d{3,4})-(?P<phone_code>\d{7,8})'
 m = re.search(p, '010-87654321')
 print(m)  # 匹配
 print(m.group())  # 返回匹配字符串
-print(m.groups())  # 获得所有组内容  一个小括号就是1组
+print(m.groups())  # 获得所有组内容
+
+# 通过组编号返回组内容
+print(m.group(1))
+print(m.group(2))
+
+# 通过组名返回组内容
+print(m.group('area_code'))
+print(m.group('phone_code'))
