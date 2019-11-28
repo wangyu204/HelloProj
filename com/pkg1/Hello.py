7502ab37eadcefacb7a814334808a10e3dbe611a
王喻 <wangyu@317hu.com>
2019-11-28 19:02:02 +0800

    修改内容：XPath表达式使用

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -3,14 +3,35 @@
 
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('data/Notes.xml')  # 创建XML文档树
-print(type(tree))  # xml.etree.ElementTree.ElementTree
+tree = ET.parse('data/Notes.xml')
+root = tree.getroot()
 
-root = tree.getroot()  # root是根元素
-print(type(root))  # xml.etree.ElementTree.Element
-print(root.tag)  # Notes
+node = root.find("./Note")  # 当前节点下的第一个Note子节点
+print(node.tag, node.attrib)
+print('--------')
+node = root.find("./Note/CDate")  # Note子节点下的第一个CDate节点
+print(node.text)
+node = root.find("./Note/CDate/..")  # Note节点
+print(node.tag, node.attrib)
 
-for index, child in enumerate(root):
-    print('第{0}个{1}元素，属性：{2}'.format(index, child.tag, child.attrib))
-    for i, child_child in enumerate(child):
-        print('    标签：{0}，内容：{1}'.format(child_child.tag, child_child.text))
+print('--------')
+node = root.find(".//CDate")  # 当前节点查找所有后代节点中第一个CDate节点
+print(node.text)
+
+print('--------1')
+node = root.find("./Note[@id]")  # 具有id属性Note节点
+print(node.tag, node.attrib)
+
+print('--------2')
+node = root.find("./Note[@id='2']")  # id属性等于'2'的Note节点
+print(node.tag, node.attrib)
+
+print('--------3')
+node = root.find("./Note[2]")  # 第二个Note节点
+print(node.tag, node.attrib)
+
+node = root.find("./Note[last()]")  # 最后一个Note节点
+print(node.tag, node.attrib)
+
+node = root.find("./Note[last()-2]")  # 倒数第三个Note节点
+print(node.tag, node.attrib)
