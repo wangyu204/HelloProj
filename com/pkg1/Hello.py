d319fd2aeb95d4da53191f7f388f8ab551602cd7
王喻 <wangyu@317hu.com>
2019-11-28 11:12:54 +0800

    修改内容：正则表达式 findall() finditer()

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -3,25 +3,12 @@
 
 import re
 
-p = r'\w+@zhijieketang\.com'
+p = r'[Jj]ava'
+text = 'I like Java and java.'
 
-text = "Tony's email is tony_guan588@zhijieketang.com."
-m = re.search(p, text)
-print(m)  # 匹配
+match_list = re.findall(p, text)
+print(match_list)  # 匹配
 
-m = re.match(p, text)
-print(m)  # 不匹配
-
-email = 'tony_guan588@zhijieketang.com'
-m = re.search(p, email)
-print(m)  # 匹配
-
-m = re.match(p, email)
-print(m)  # 匹配
-
-# match对象几个方法
-print('match对象几个方法:')
-print(m.group())  # tony_guan588@zhijieketang.com
-print(m.start())  # 0
-print(m.end())  # 29
-print(m.span())  # (0, 29)
+match_iter = re.finditer(p, text)
+for m in match_iter:
+    print(m.group())
