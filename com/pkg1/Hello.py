df4845b61e8a451388b76df4a9544da638211106
王喻 <wangyu@317hu.com>
2019-11-28 15:35:21 +0800

    修改内容：正则表达式 compile() 指定编码格式

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -3,33 +3,21 @@
 
 import re
 
-p = r'\w+@zhijieketang\.com'
-regex = re.compile(p)
+text = '你们好Hello'
 
-text = "Tony's email is tony_guan588@zhijieketang.com."
+p = r'\w+'
+regex = re.compile(p, re.U)
+
+m = regex.search(text)
+print(m)  # 匹配
+
+m = regex.match(text)
+print(m)  # 匹配
+
+regex = re.compile(p, re.A)
+
 m = regex.search(text)
 print(m)  # 匹配
 
 m = regex.match(text)
 print(m)  # 不匹配
-
-p = r'[Jj]ava'
-regex = re.compile(p)
-text = 'I like Java and java.'
-
-match_list = regex.findall(text)
-print(match_list)  # 匹配
-
-match_iter = regex.finditer(text)
-for m in match_iter:
-    print(m.group())
-
-p = r'\d+'
-regex = re.compile(p)
-text = 'AB12CD34EF'
-
-clist = regex.split(text)
-print(clist)
-
-repace_text = regex.sub(' ', text)
-print(repace_text)
