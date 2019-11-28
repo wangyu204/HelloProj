93cce9b20c67235984a126d207c6d14847bb175c
王喻 <wangyu@317hu.com>
2019-11-28 19:14:57 +0800

    修改内容：JSON 编码 dumps()  dump()   将字典编码成str 或 保存到文件中

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,37 +1,35 @@
 # coding=utf-8
 
 
-import xml.etree.ElementTree as ET
+import json
 
-tree = ET.parse('data/Notes.xml')
-root = tree.getroot()
+# 准备数据
+py_dict = {'name': 'tony', 'age': 30, 'sex': True}  # 创建字典对象
+py_list = [1, 3]  # 创建列表对象
+py_tuple = ('A', 'B', 'C')  # 创建元组对象
 
-node = root.find("./Note")  # 当前节点下的第一个Note子节点
-print(node.tag, node.attrib)
-print('--------')
-node = root.find("./Note/CDate")  # Note子节点下的第一个CDate节点
-print(node.text)
-node = root.find("./Note/CDate/..")  # Note节点
-print(node.tag, node.attrib)
+py_dict['a'] = py_list  # 添加列表到字典中
+py_dict['b'] = py_tuple  # 添加元组到字典中
 
-print('--------')
-node = root.find(".//CDate")  # 当前节点查找所有后代节点中第一个CDate节点
-print(node.text)
+print(py_dict)
+print(type(py_dict))  # <class 'dict'>
 
-print('--------1')
-node = root.find("./Note[@id]")  # 具有id属性Note节点
-print(node.tag, node.attrib)
+# 编码过程
+print('---------')
+json_obj = json.dumps(py_dict)
+print(json_obj)
+print(type(json_obj))  # <class 'str'>
 
-print('--------2')
-node = root.find("./Note[@id='2']")  # id属性等于'2'的Note节点
-print(node.tag, node.attrib)
+# 编码过程
+print('---------')
+json_obj = json.dumps(py_dict, indent=2)
+# 输出格式化后的字符串
+print(json_obj)
 
-print('--------3')
-node = root.find("./Note[2]")  # 第二个Note节点
-print(node.tag, node.attrib)
+# 写入JSON数据到data1.json文件
+with open('data/data1.json', 'w') as f:
+    json.dump(py_dict, f)
 
-node = root.find("./Note[last()]")  # 最后一个Note节点
-print(node.tag, node.attrib)
-
-node = root.find("./Note[last()-2]")  # 倒数第三个Note节点
-print(node.tag, node.attrib)
+# 写入JSON数据到data2.json文件
+with open('data/data2.json', 'w') as f:
+    json.dump(py_dict, f, indent=2)
