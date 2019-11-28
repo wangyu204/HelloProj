b6a26490fccb227d5cca22131fd5c2407541e5ed
王喻 <wangyu@317hu.com>
2019-11-28 19:39:15 +0800

    修改内容：配置文件ini 写入  write()

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -7,26 +7,12 @@ config = configparser.ConfigParser()  # 创建配置解析器对象
 
 config.read('data/Setup.ini', encoding='utf-8')  # 读取并解析配置文件
 
-print(config.sections())  # 返回所有的节
+# 写入配置文件
+config['Startup']['RequireMSI'] = '8.0'
+config['Product']['RequireMSI'] = '4.0'
 
-print('-------')
-section1 = config['Startup']  # 返回Startup节
-print(config.options('Startup'))
+config.add_section('Section2')  # 添加节
+config.set('Section2', 'name', 'Mac')  # 添加配置项
 
-print('-------')
-print(section1['requireOS'])
-print(section1['RequireIE'])
-
-print('-------')
-print(config['Product']['msi'])
-
-print('-------')
-print(config['Windows 2000']['MajorVersion'])  # 返回MajorVersion数据
-print(config['Windows 2000']['ServicePackMajor'])
-
-print('-------')
-value = config.get('Windows 2000', 'MajorVersion')  # 返回MajorVersion数据
-print(type(value))  # <class 'str'>
-
-value = config.getint('Windows 2000', 'MajorVersion')  # 返回MajorVersion数据
-print(type(value))  # <class 'int'>
+with open('data/Setup.ini', 'w') as fw:
+    config.write(fw)
