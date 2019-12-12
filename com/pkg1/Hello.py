e09a66c008a7e335f7321c80ad6328b8a2bbee0e
王喻 <wangyu@317hu.com>
2019-12-12 19:30:15 +0800

    修改内容：数据库操作-改

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -2,38 +2,6 @@
 
 import pymysql
 
-
-# 查询最大用户Id
-def read_max_userid():
-    # 1. 建立数据库连接
-    connection = pymysql.connect(host='localhost',
-                                 user='root',
-                                 password='wy123456',
-                                 database='MyDB',
-                                 charset='utf8')
-
-    try:
-        # 2. 创建游标对象
-        with connection.cursor() as cursor:
-
-            # 3. 执行SQL操作
-            sql = 'select max(userid) from user'
-            cursor.execute(sql)
-
-            # 4. 提取结果集
-            row = cursor.fetchone()
-
-            if row is not None:
-                print('最大用户Id ：{0}'.format(row[0]))
-                return row[0]
-
-        # with代码块结束 5. 关闭游标
-
-    finally:
-        # 6. 关闭数据连接
-        connection.close()
-
-
 # 1. 建立数据库连接
 connection = pymysql.connect(host='localhost',
                              user='root',
@@ -41,18 +9,13 @@ connection = pymysql.connect(host='localhost',
                              database='MyDB',
                              charset='utf8')
 
-# 查询最大值
-maxid = read_max_userid()
-
 try:
     # 2. 创建游标对象
     with connection.cursor() as cursor:
 
         # 3. 执行SQL操作
-        sql = 'insert into user (userid, name) values (%s,%s)'
-        nextid = maxid + 1
-        name = 'Tony' + str(nextid)
-        affectedcount = cursor.execute(sql, (nextid, name))
+        sql = 'update user set name = %s where userid > %s'
+        affectedcount = cursor.execute(sql, ('Tom', 2))
 
         print('影响的数据行数：{0}'.format(affectedcount))
         # 4. 提交数据库事物
@@ -60,9 +23,10 @@ try:
 
     # with代码块结束 5. 关闭游标
 
-except pymysql.DatabaseError:
+except pymysql.DatabaseError as e:
     # 4. 回滚数据库事物
     connection.rollback()
+    print(e)
 finally:
     # 6. 关闭数据连接
     connection.close()
