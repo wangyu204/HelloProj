6d089c84a5be3983b47d94db13c72df5d823f5bc
王喻 <wangyu@317hu.com>
2019-12-15 20:14:00 +0800

    修改内容：1对1事件处理

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -7,10 +7,16 @@ import wx
 # 自定义窗口类MyFrame
 class MyFrame(wx.Frame):
     def __init__(self):
-        super().__init__(parent=None, title="第一个GUI程序!", size=(400, 300))
+        super().__init__(parent=None, title='一对一事件处理', size=(300, 180))
         self.Centre()  # 设置窗口居中
         panel = wx.Panel(parent=self)
-        statictext = wx.StaticText(parent=panel, label='Hello World!', pos=(10, 10))
+        self.statictext = wx.StaticText(parent=panel, pos=(110, 20))
+        b = wx.Button(parent=panel, label='OK', pos=(100, 50))
+        self.Bind(wx.EVT_BUTTON, self.on_click, b)
+
+    def on_click(self, event):
+        print(type(event))  # <class 'wx._core.CommandEvent'>
+        self.statictext.SetLabelText('Hello, world.')
 
 
 class App(wx.App):
