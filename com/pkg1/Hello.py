07a3c64f5101bb874b4c1e78adbf2fdb2ef6fd8b
王喻 <wangyu@317hu.com>
2019-12-15 19:54:53 +0800

    修改内容：自定义frame和app（带生命周期）

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -2,11 +2,27 @@
 
 import wx
 
-# 创建应用程序对象
-app = wx.App()
-# 创建窗口对象
-frm = wx.Frame(None, title="第一个GUI程序!", size=(400, 300), pos=(100, 100))
 
-frm.Show()  # 显示窗口
+# 自定义窗口类MyFrame
+class MyFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None, title="第一个GUI程序!", size=(400, 300), pos=(100, 100))
+        # TODO
 
-app.MainLoop()  # 进入主事件循环
+
+class App(wx.App):
+
+    def OnInit(self):
+        # 创建窗口对象
+        frame = MyFrame()
+        frame.Show()
+        return True
+
+    def OnExit(self):
+        print('应用程序退出')
+        return 0
+
+
+if __name__ == '__main__':
+    app = App()
+    app.MainLoop()  # 进入主事件循环
