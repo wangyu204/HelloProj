070a74c9d1a011d42d462d801b2958853cfbc45d
王喻 <wangyu@317hu.com>
2019-12-15 19:59:04 +0800

    修改内容：通用例子

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,13 +1,16 @@
 # coding=utf-8
 
+
 import wx
 
 
 # 自定义窗口类MyFrame
 class MyFrame(wx.Frame):
     def __init__(self):
-        super().__init__(parent=None, title="第一个GUI程序!", size=(400, 300), pos=(100, 100))
-        # TODO
+        super().__init__(parent=None, title="第一个GUI程序!", size=(400, 300))
+        self.Centre()  # 设置窗口居中
+        panel = wx.Panel(parent=self)
+        statictext = wx.StaticText(parent=panel, label='Hello World!', pos=(10, 10))
 
 
 class App(wx.App):
@@ -17,10 +20,6 @@ class App(wx.App):
         frame = MyFrame()
         frame.Show()
         return True
-
-    def OnExit(self):
-        print('应用程序退出')
-        return 0
 
 
 if __name__ == '__main__':
