55b1617a6b7afaade633014b50452127c7c501d9
王喻 <wangyu@317hu.com>
2019-12-15 11:40:11 +0800

    修改内容：GET请求

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,8 +1,19 @@
 # coding=utf-8
 
+import urllib.parse
 import urllib.request
 
-with urllib.request.urlopen('http://www.sina.com.cn/') as response:
+url = 'http://www.51work6.com/service/mynotes/WebService.php'
+email = '414875346@qq.com'
+params_dict = {'email': email, 'type': 'JSON', 'action': 'query'}
+params_str = urllib.parse.urlencode(params_dict)
+print(params_str)
+
+url = url + '?' + params_str  # HTTP参数放到URL之后
+print(url)
+
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
     data = response.read()
-    html = data.decode()
-    print(html)
+    json_data = data.decode()
+    print(json_data)
