2a36656ab3a316b4856e2fdb15f17510e0e763f7
王喻 <wangyu@317hu.com>
2019-12-15 11:08:22 +0800

    修改内容：UDP Socket低层次网络编程-文件上传工具

---
+++ com/pkg1/udp-upload-server.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+
+import socket
+
+HOST = '127.0.0.1'
+PORT = 8888
+
+f_name = 'test_copy.txt'
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.bind((HOST, PORT))
+    print('服务器启动...')
+
+    # 创建字节序列对象列表，作为接收数据的缓冲区
+    buffer = []
+    while True:  # 反复接收数据
+        data, _ = s.recvfrom(1024)
+        if data:
+            flag = data.decode()
+            if flag == 'bye':  # 和客户端约定好的结束符号
+                break
+            buffer.append(data)
+        else:
+            # 没有接收到数据，进入下次循环继续接收
+            continue
+    # 将接收的字节序列对象列表合并为一个字节序列对象
+    b = bytes().join(buffer)
+    with open(f_name, 'w') as f:
+        f.write(b.decode())
+
+    print('服务器接收完成。')
