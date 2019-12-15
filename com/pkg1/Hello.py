bdb40737b193d5f87711cd0cf404ec75734a3f56
王喻 <wangyu@317hu.com>
2019-12-15 11:43:21 +0800

    修改内容：POST请求

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -4,15 +4,14 @@ import urllib.parse
 import urllib.request
 
 url = 'http://www.51work6.com/service/mynotes/WebService.php'
+# 准备HTTP参数
 email = '414875346@qq.com'
 params_dict = {'email': email, 'type': 'JSON', 'action': 'query'}
 params_str = urllib.parse.urlencode(params_dict)
 print(params_str)
+params_bytes = params_str.encode()  # 字符串转换为字节序列对象
 
-url = url + '?' + params_str  # HTTP参数放到URL之后
-print(url)
-
-req = urllib.request.Request(url)
+req = urllib.request.Request(url, data=params_bytes)  # 发送POST请求
 with urllib.request.urlopen(req) as response:
     data = response.read()
     json_data = data.decode()
