951e126e2429b399ab82dceab2d497a4353cb88f
王喻 <wangyu@317hu.com>
2019-12-18 19:23:28 +0800

    修改内容：复选框和单选按钮

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -6,37 +6,65 @@ import wx
 # 自定义窗口类MyFrame
 class MyFrame(wx.Frame):
     def __init__(self):
-        super().__init__(parent=None, title='文本框', size=(400, 200))
+        super().__init__(parent=None, title='复选框和单选按钮', size=(400, 130))
         self.Centre()  # 设置窗口居中
         panel = wx.Panel(self)
 
-        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        hbox1 = wx.BoxSizer(wx.HORIZONTAL)
 
-        fgs = wx.FlexGridSizer(3, 2, 10, 10)
+        statictext = wx.StaticText(panel, label='选择你喜欢的编程语言：')
+        cb1 = wx.CheckBox(panel, 1, 'Python') //id
+        cb2 = wx.CheckBox(panel, 2, 'Java')
+        cb2.SetValue(True)
+        cb3 = wx.CheckBox(panel, 3, 'C++')
+        self.Bind(wx.EVT_CHECKBOX, self.on_checkbox_click, id=1, id2=3)
 
-        userid = wx.StaticText(panel, label="用户ID：")
-        pwd = wx.StaticText(panel, label="密码：")
-        content = wx.StaticText(panel, label="多行文本：")
+        hbox1.Add(statictext, 1, flag=wx.LEFT | wx.RIGHT | wx.FIXED_MINSIZE, border=5)
+        hbox1.Add(cb1, 1, flag=wx.ALL | wx.FIXED_MINSIZE)
+        hbox1.Add(cb2, 1, flag=wx.ALL | wx.FIXED_MINSIZE)
+        hbox1.Add(cb3, 1, flag=wx.ALL | wx.FIXED_MINSIZE)
 
-        tc1 = wx.TextCtrl(panel)
-        tc2 = wx.TextCtrl(panel, style=wx.TE_PASSWORD)
-        tc3 = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
+        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
 
-        # 设置tc1初始值
-        tc1.SetValue('tony')
-        # 获取tc1值
-        print('读取用户ID：{0}'.format(tc1.GetValue()))
+        statictext = wx.StaticText(panel, label='选择性别：')
+        radio1 = wx.RadioButton(panel, 4, '男', style=wx.RB_GROUP)
+        radio2 = wx.RadioButton(panel, 5, '女')
+        self.Bind(wx.EVT_RADIOBUTTON, self.on_radio1_click, id=4, id2=5)
 
-        fgs.AddMany([userid, (tc1, 1, wx.EXPAND),
-                     pwd, (tc2, 1, wx.EXPAND),
-                     content, (tc3, 1, wx.EXPAND)])
-        fgs.AddGrowableRow(0, 1)
-        fgs.AddGrowableRow(1, 1)
-        fgs.AddGrowableRow(2, 3)
-        fgs.AddGrowableCol(0, 1)
-        fgs.AddGrowableCol(1, 2)
-        hbox.Add(fgs, proportion=1, flag=wx.ALL | wx.EXPAND, border=15)
-        panel.SetSizer(hbox)
+        hbox2.Add(statictext, 1, flag=wx.LEFT | wx.RIGHT | wx.FIXED_MINSIZE, border=5)
+        hbox2.Add(radio1, 1, flag=wx.ALL | wx.FIXED_MINSIZE)
+        hbox2.Add(radio2, 1, flag=wx.ALL | wx.FIXED_MINSIZE)
+
+        hbox3 = wx.BoxSizer(wx.HORIZONTAL)
+
+        statictext = wx.StaticText(panel, label='选择你最喜欢吃的水果：')
+        radio3 = wx.RadioButton(panel, 6, '苹果', style=wx.RB_GROUP)
+        radio4 = wx.RadioButton(panel, 7, '橘子')
+        radio5 = wx.RadioButton(panel, 8, '香蕉')
+        self.Bind(wx.EVT_RADIOBUTTON, self.on_radio2_click, id=6, id2=8)
+
+        hbox3.Add(statictext, 1, flag=wx.LEFT | wx.RIGHT | wx.FIXED_MINSIZE, border=5)
+        hbox3.Add(radio3, 1, flag=wx.ALL | wx.FIXED_MINSIZE)
+        hbox3.Add(radio4, 1, flag=wx.ALL | wx.FIXED_MINSIZE)
+        hbox3.Add(radio5, 1, flag=wx.ALL | wx.FIXED_MINSIZE)
+
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        vbox.Add(hbox1, 1, flag=wx.ALL | wx.EXPAND, border=5)
+        vbox.Add(hbox2, 1, flag=wx.ALL | wx.EXPAND, border=5)
+        vbox.Add(hbox3, 1, flag=wx.ALL | wx.EXPAND, border=5)
+        panel.SetSizer(vbox)
+
+    def on_checkbox_click(self, event):
+        cb = event.GetEventObject()
+        print('选择 {0}，状态{1}'.format(cb.GetLabel(), event.IsChecked()))
+
+    def on_radio1_click(self, event):
+        rb = event.GetEventObject()
+        print('第一组 {0} 被选中'.format(rb.GetLabel()))
+
+    def on_radio2_click(self, event):
+        rb = event.GetEventObject()
+        print('第二组 {0} 被选中'.format(rb.GetLabel()))
 
 
 class App(wx.App):
