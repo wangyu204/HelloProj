ec175453085aa4b9d474c59776ad0415f9fabc93
王喻 <wangyu@317hu.com>
2019-12-18 18:44:15 +0800

    修改内容：Grid布局器使用

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -7,40 +7,44 @@ import wx
 # 自定义窗口类MyFrame
 class MyFrame(wx.Frame):
     def __init__(self):
-        super().__init__(parent=None, title='StaticBox布局', size=(300, 120))
+        super().__init__(parent=None, title='Grid布局', size=(300, 300))
         self.Centre()  # 设置窗口居中
-        panel = wx.Panel(parent=self)
-        # 创建垂直方向的Box布局管理器对象
-        vbox = wx.BoxSizer(wx.VERTICAL)
-        self.statictext = wx.StaticText(parent=panel, label='Button1单击')
-        # 添加静态文本到Box布局管理器
-        vbox.Add(self.statictext, proportion=2, flag=wx.FIXED_MINSIZE | wx.TOP | wx.CENTER, border=10)
+        panel = wx.Panel(self)
+        btn1 = wx.Button(panel, label='1')
+        btn2 = wx.Button(panel, label='2')
+        btn3 = wx.Button(panel, label='3')
+        btn4 = wx.Button(panel, label='4')
+        btn5 = wx.Button(panel, label='5')
+        btn6 = wx.Button(panel, label='6')
+        btn7 = wx.Button(panel, label='7')
+        btn8 = wx.Button(panel, label='8')
+        btn9 = wx.Button(panel, label='9')
 
-        b1 = wx.Button(parent=panel, id=10, label='Button1')
-        b2 = wx.Button(parent=panel, id=11, label='Button2')
-        self.Bind(wx.EVT_BUTTON, self.on_click, id=10, id2=20)
+        grid = wx.GridSizer(cols=3, rows=3, vgap=0, hgap=20)
 
-        # 创建静态框对象
-        sb = wx.StaticBox(panel, label="按钮框")
-        # 创建水平方向的StaticBox布局管理器
-        hsbox = wx.StaticBoxSizer(sb, wx.HORIZONTAL)
-        # 添加b1到水平StaticBox布局管理
-        hsbox.Add(b1, 0, wx.EXPAND | wx.BOTTOM, 5)
-        # 添加b2到水平StaticBox布局管理
-        hsbox.Add(b2, 0, wx.EXPAND | wx.BOTTOM, 5)
+        # grid.AddMany([
+        #     (btn1, 0, wx.EXPAND),
+        #     (btn2, 0, wx.EXPAND),
+        #     (btn3, 0, wx.EXPAND),
+        #     (btn4, 0, wx.EXPAND),
+        #     (btn5, 0, wx.EXPAND),
+        #     (btn6, 0, wx.EXPAND),
+        #     (btn7, 0, wx.EXPAND),
+        #     (btn8, 0, wx.EXPAND),
+        #     (btn9, 0, wx.EXPAND)
+        # ])
 
-        # 添加hbox到vbox
-        vbox.Add(hsbox, proportion=1, flag=wx.CENTER)
+        grid.Add(btn1, 0, wx.EXPAND)
+        grid.Add(btn2, 0, wx.EXPAND)
+        grid.Add(btn3, 0, wx.EXPAND)
+        grid.Add(btn4, 0, wx.EXPAND)
+        grid.Add(btn5, 0, wx.EXPAND)
+        grid.Add(btn6, 0, wx.EXPAND)
+        grid.Add(btn7, 0, wx.EXPAND)
+        grid.Add(btn8, 0, wx.EXPAND)
+        grid.Add(btn9, 0, wx.EXPAND)
 
-        panel.SetSizer(vbox)
-
-    def on_click(self, event):
-        event_id = event.GetId()
-        print(event_id)
-        if event_id == 10:
-            self.statictext.SetLabelText('Button1单击')
-        else:
-            self.statictext.SetLabelText('Button2单击')
+        panel.SetSizer(grid)
 
 
 class App(wx.App):
