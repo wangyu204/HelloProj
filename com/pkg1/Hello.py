91615e831601ae3c839fac0418dbde32bb164610
王喻 <wangyu@317hu.com>
2019-12-19 17:01:57 +0800

    修改内容：多线程-临界资源问题-销售机票例子（错误的例子）

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -4,31 +4,65 @@
 import threading
 import time
 
-# 线程停止变量
-isrunning = True
+
+class TicketDB:
+    def __init__(self):
+        # 机票的数量
+        self.ticket_count = 5
+
+    # 获得当前机票数量
+    def get_ticket_count(self):
+        return self.ticket_count
+
+    # 销售机票
+    def sell_ticket(self):
+        # TODO 等于用户付款
+        # 线程休眠，阻塞当前线程，模拟等待用户付款
+        time.sleep(1)
+        print("第{0}号票,已经售出".format(self.ticket_count))
+        self.ticket_count -= 1
 
 
-# 线程体函数
-def thread_body():
-    while isrunning:
-        # 线程开始工作
-        print('下载中...')
-        # 线程休眠
-        time.sleep(5)
-    print('执行完成!')
+# 创建TicketDB对象
+db = TicketDB()
+
+
+# 线程体1函数
+def thread1_body():
+    global db  # 声明为全局变量
+    while True:
+        curr_ticket_count = db.get_ticket_count()
+        # 查询是否有票
+        if curr_ticket_count > 0:
+            db.sell_ticket()
+        else:
+            # 无票退出
+            break
+
+
+# 线程体2函数
+def thread2_body():
+    global db  # 声明为全局变量
+    while True:
+        curr_ticket_count = db.get_ticket_count()
+        # 查询是否有票
+        if curr_ticket_count > 0:
+            db.sell_ticket()
+        else:
+            # 无票退出
+            break
 
 
 # 主函数
 def main():
     # 创建线程对象t1
-    t1 = threading.Thread(target=thread_body)
+    t1 = threading.Thread(target=thread1_body)
     # 启动线程t1
     t1.start()
-    # 从键盘输入停止指令 exit
-    command = input('请输入停止指令：')
-    if command == 'exit':
-        global isrunning
-        isrunning = False
+    # 创建线程对象t2
+    t2 = threading.Thread(target=thread2_body)
+    # 启动线程t2
+    t2.start()
 
 
 if __name__ == '__main__':
