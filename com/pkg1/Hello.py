6dece6e2d3239d76b3ddaa4cde9cbbd5e4c55df8
王喻 <wangyu@317hu.com>
2019-12-19 14:04:05 +0800

    修改内容：工具栏

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 
+
 import wx
 import wx.grid
 
@@ -7,8 +8,9 @@ import wx.grid
 # 自定义窗口类MyFrame
 class MyFrame(wx.Frame):
     def __init__(self):
-        super().__init__(parent=None, title='使用菜单', size=(550, 500))
+        super().__init__(parent=None, title='使用工具栏', size=(550, 500))
         self.Centre()  # 设置窗口居中
+        self.Show(True)
 
         self.text = wx.TextCtrl(self, -1, style=wx.EXPAND | wx.TE_MULTILINE)
         vbox = wx.BoxSizer(wx.VERTICAL)
@@ -19,7 +21,6 @@ class MyFrame(wx.Frame):
 
         file_menu = wx.Menu()
         new_item = wx.MenuItem(file_menu, wx.ID_NEW, text="新建", kind=wx.ITEM_NORMAL)
-        self.Bind(wx.EVT_MENU, self.on_newitem_click, id=wx.ID_NEW)
         file_menu.Append(new_item)
         file_menu.AppendSeparator()
 
@@ -33,24 +34,39 @@ class MyFrame(wx.Frame):
         paste_item = wx.MenuItem(edit_menu, 102, text="粘贴", kind=wx.ITEM_NORMAL)
         edit_menu.Append(paste_item)
 
-        self.Bind(wx.EVT_MENU, self.on_editmenu_click, id=100, id2=102)
-
         file_menu.AppendSubMenu(edit_menu, "编辑")
 
         menubar.Append(file_menu, '文件')
         self.SetMenuBar(menubar)
 
-    def on_newitem_click(self, event):
-        self.text.SetLabel('单击【新建】菜单')
+        tb = wx.ToolBar(self, wx.ID_ANY)
+        self.ToolBar = tb
+        tsize = (24, 24)
+        new_bmp = wx.ArtProvider.GetBitmap(wx.ART_NEW, wx.ART_TOOLBAR, tsize)
+        open_bmp = wx.ArtProvider.GetBitmap(wx.ART_FILE_OPEN, wx.ART_TOOLBAR, tsize)
+        copy_bmp = wx.ArtProvider.GetBitmap(wx.ART_COPY, wx.ART_TOOLBAR, tsize)
+        paste_bmp = wx.ArtProvider.GetBitmap(wx.ART_PASTE, wx.ART_TOOLBAR, tsize)
 
-    def on_editmenu_click(self, event):
+        tb.AddTool(10, "New", new_bmp, kind=wx.ITEM_NORMAL, shortHelp="New")
+        tb.AddTool(20, "Open", open_bmp, kind=wx.ITEM_NORMAL, shortHelp="Open")
+        tb.AddSeparator()
+        tb.AddTool(30, "Copy", copy_bmp, kind=wx.ITEM_NORMAL, shortHelp="Copy")
+        tb.AddTool(40, "Paste", paste_bmp, kind=wx.ITEM_NORMAL, shortHelp="Paste")
+        tb.AddSeparator()
+
+        tb.AddTool(201, "back", wx.Bitmap("menu_icon/back.png"), kind=wx.ITEM_NORMAL, shortHelp="Back")
+        tb.AddTool(202, "forward", wx.Bitmap("menu_icon/forward.png"), kind=wx.ITEM_NORMAL, shortHelp="Forward")
+        self.Bind(wx.EVT_MENU, self.on_click, id=201, id2=202)
+        tb.AddSeparator()
+
+        tb.Realize()
+
+    def on_click(self, event):
         event_id = event.GetId()
-        if event_id == 100:
-            self.text.SetLabel('单击【复制】菜单')
-        elif event_id == 101:
-            self.text.SetLabel('单击【剪切】菜单')
+        if event_id == 201:
+            self.text.SetLabel('单击【Back】按钮')
         else:
-            self.text.SetLabel('单击【粘贴】菜单')
+            self.text.SetLabel('单击【Forward】按钮')
 
 
 class App(wx.App):
