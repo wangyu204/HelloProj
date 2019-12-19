dbbbf76f601d203690c2e11939c750ba2a84fae3
王喻 <wangyu@317hu.com>
2019-12-19 14:28:59 +0800

    修改内容：多线程-方法1创建线程

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,16 +1,33 @@
 # coding=utf-8
 
 import threading
+import time
 
-# 当前线程对象
-t = threading.current_thread()
-# 当前线程名
-print(t.name)
 
-# 返回当前处于活动状态的线程个数
-print(threading.active_count())
+# 线程体函数
+def thread_body():
+    # 当前线程对象
+    t = threading.current_thread()
+    for n in range(2):
+        # 当前线程名
+        print('第{0}次执行线程{1}'.format(n, t.name))
+        # 线程休眠
+        time.sleep(1)
+    print('线程{0}执行完成！'.format(t.name))
 
-# 当主线程对象
-t = threading.main_thread()
-# 主线程名
-print(t.name)
+
+# 主函数
+def main():
+    # 创建线程对象t1
+    t1 = threading.Thread(target=thread_body)
+    # 启动线程t1
+    t1.start()
+
+    # 创建线程对象t2
+    t2 = threading.Thread(target=thread_body, name='MyThread')
+    # 启动线程t2
+    t2.start()
+
+
+if __name__ == '__main__':
+    main()
