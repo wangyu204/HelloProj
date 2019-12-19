2fae04c9d99d947d0a47d7eedff4e779af4aeb1c
王喻 <wangyu@317hu.com>
2019-12-19 14:30:39 +0800

    修改内容：多线程-方法2创建线程 （继承线程类）

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -4,27 +4,31 @@ import threading
 import time
 
 
-# 线程体函数
-def thread_body():
-    # 当前线程对象
-    t = threading.current_thread()
-    for n in range(2):
-        # 当前线程名
-        print('第{0}次执行线程{1}'.format(n, t.name))
-        # 线程休眠
-        time.sleep(1)
-    print('线程{0}执行完成！'.format(t.name))
+class MyThread(threading.Thread):
+    def __init__(self, name=None):
+        super().__init__(name=name)
+
+    # 线程体函数
+    def run(self):
+        # 当前线程对象
+        t = threading.current_thread()
+        for n in range(2):
+            # 当前线程名
+            print('第{0}次执行线程{1}'.format(n, t.name))
+            # 线程休眠
+            time.sleep(1)
+        print('线程{0}执行完成！'.format(t.name))
 
 
 # 主函数
 def main():
     # 创建线程对象t1
-    t1 = threading.Thread(target=thread_body)
+    t1 = MyThread()
     # 启动线程t1
     t1.start()
 
     # 创建线程对象t2
-    t2 = threading.Thread(target=thread_body, name='MyThread')
+    t2 = MyThread(name='MyThread')
     # 启动线程t2
     t2.start()
 
