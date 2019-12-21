99305675d437f6ad5362ca2bae8540dc52b09d84
王喻 <wangyu@317hu.com>
2019-12-21 09:44:56 +0800

    修改内容：使用Matplotlib绘制图表-柱状图

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,24 +1,26 @@
 # coding=utf-8
 
+
 import matplotlib.pyplot as plt
 
 # 设置中文字体
 plt.rcParams['font.family'] = ['SimHei']
 
-x = [5, 4, 2, 1]  # x轴坐标数据
-y = [7, 8, 9, 10]  # y轴坐标数据
+x1 = [1, 3, 5, 7, 9]  # x1轴坐标数据
+y1 = [5, 2, 7, 8, 2]  # y1轴坐标数据
 
-# 绘制线段
-plt.plot(x, y, 'b', label='线1', linewidth=2)
+x2 = [2, 4, 6, 8, 10]  # x2轴坐标数据
+y2 = [8, 6, 2, 5, 6]  # y2轴坐标数据
 
-plt.title('绘制折线图')  # 添加图表标题
+# 绘制柱状图
+plt.bar(x1, y1, label='柱状图1')
+plt.bar(x2, y2, label='柱状图2')
+
+plt.title('绘制柱状图')  # 添加图表标题
 
 plt.ylabel('y轴')  # 添加y轴标题
 plt.xlabel('x轴')  # 添加x轴标题
 
 plt.legend()  # 设置图例
-
-# 以分辨率 72 来保存图片
-plt.savefig('折线图', dpi=72)
 
 plt.show()  # 显示图形
