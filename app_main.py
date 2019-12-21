f385a7465e3f8d7d0e92d7f715349134935fbfb3
王喻 <wangyu@317hu.com>
2019-12-21 16:30:27 +0800

    修改内容：项目3petstore-表示层-用户登录界面

--- app_main.py
+++ app_main.py
@@ -2,12 +2,15 @@
 
 import wx
 
+from com.ewell.petstore.ui.login_frame import LoginFrame
+
 
 class App(wx.App):
 
     def OnInit(self):
         # 创建窗口对象
-
+        frame = LoginFrame()
+        frame.Show()
         return True
 
 
