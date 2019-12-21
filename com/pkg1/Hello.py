2fa1fdc66c2957e452a92a178f90e68a4012eef6
王喻 <wangyu@317hu.com>
2019-12-21 09:50:08 +0800

    修改内容：使用Matplotlib绘制图表-饼状图

--- com/pkg1/Hello.py
+++ com/pkg1/Hello.py
@@ -1,26 +1,20 @@
 # coding=utf-8
 
-
 import matplotlib.pyplot as plt
 
 # 设置中文字体
 plt.rcParams['font.family'] = ['SimHei']
 
-x1 = [1, 3, 5, 7, 9]  # x1轴坐标数据
-y1 = [5, 2, 7, 8, 2]  # y1轴坐标数据
+# 各种活动标题列表
+activies = ['工作', '睡', '吃', '玩']
+# 各种活动所占时间列表
+slices = [8, 7, 3, 6]
+# 各种活动在饼状图中的颜色列表
+cols = ['c', 'm', 'r', 'b']
 
-x2 = [2, 4, 6, 8, 10]  # x2轴坐标数据
-y2 = [8, 6, 2, 5, 6]  # y2轴坐标数据
+plt.pie(slices, labels=activies, colors=cols,
+        shadow=True, explode=(0, 0.1, 0, 0), autopct='%.1f%%')
 
-# 绘制柱状图
-plt.bar(x1, y1, label='柱状图1')
-plt.bar(x2, y2, label='柱状图2')
-
-plt.title('绘制柱状图')  # 添加图表标题
-
-plt.ylabel('y轴')  # 添加y轴标题
-plt.xlabel('x轴')  # 添加x轴标题
-
-plt.legend()  # 设置图例
+plt.title('绘制饼状图')
 
 plt.show()  # 显示图形
